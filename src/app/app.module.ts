05aa5df5ca094ef8622a5312a30e00501570a9b0
bpapps <ia.ansari.in@gmail.com>
2018-02-24 23:59:28 +0530

    Routing =========== Summary
        You added the Angular router to navigate among different components.
        You turned the AppComponent into a navigation shell with <a> links
    and a <router-outlet>.
        You configured the router in an AppRoutingModule
        You defined simple routes, a redirect route, and a parameterized
    route.
        You used the routerLink directive in anchor elements.
        You refactored a tightly-coupled master/detail view into a routed
    detail view.
        You used router link parameters to navigate to the detail view of a
    user-selected hero.
        You shared the HeroService among multiple components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,21 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent //Every component must be declared in exactly one NgModule.
+    MessagesComponent,
+    DashboardComponent //Every component must be declared in exactly one NgModule.
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   //The providers array tells Angular to create a single, shared instance of HeroService and inject into any class that asks for it.
   providers: [
