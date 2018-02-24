511bf07f78d3678cf3e3c39a3e4f5f37faf556ae
bpapps <ia.ansari.in@gmail.com>
2018-02-24 21:15:48 +0530

    Services =============== Summary
        You refactored data access to the HeroService class.
        You provided the HeroService in the root AppModule so that it can be
    injected anywhere.
        You used Angular Dependency Injection to inject it into a component.
        You gave the HeroService get data method an asynchronous signature.
        You discovered Observable and the RxJS Observable library.
        You used RxJS of() to return an Observable of mock heroes
    (Observable<Hero[]>).
        The component's ngOnInit lifecycle hook calls the HeroService
    method, not the constructor.
        You created a MessageService for loosely-coupled communication
    between classes.
        The HeroService injected into a component is created with another
    injected service, MessageService.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,26 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent //Every component must be declared in exactly one NgModule.
+    HeroDetailComponent,
+    MessagesComponent //Every component must be declared in exactly one NgModule.
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  //The providers array tells Angular to create a single, shared instance of HeroService and inject into any class that asks for it.
+  providers: [
+    HeroService,
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+// import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 /**
 You always import the Component symbol from the Angular core library
@@ -23,20 +24,42 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
 
   // list of heros
-  heroes = HEROES;
+  heroes: Hero[];
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
 
+/**
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
+*/
 
-  constructor() { }
+/**
+The new version waits for the Observable to emit the array of heroes— which could happen now
+ or several minutes from now. Then subscribe passes the emitted array to the callback, which
+ sets the component's heroes property.
+*/
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) {
+    /**
+    The parameter simultaneously defines a private heroService property and identifies it as a HeroService injection site.
+    When Angular creates a HeroesComponent, the Dependency Injection system sets the heroService parameter to the singleton instance of HeroService.
+    */
+   }
 
   ngOnInit() {
     /**
     The ngOnInit is a lifecycle hook Angular calls ngOnInit shortly after creating a component.
     It's a good place to put initialization logic.
     */
+    this.getHeroes();
+
   }
 
 }
